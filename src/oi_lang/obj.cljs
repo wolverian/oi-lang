3b0b184c0cfac46f1ca3d4617a7987ea08732f37
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2017-04-30 14:49:25 +0300

    strings

--- src/oi_lang/obj.cljs
+++ src/oi_lang/obj.cljs
@@ -9,13 +9,16 @@
    number = digit+
    <digit> = '0'|'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9'
 
-   string = '\"'
+   string = <'\"'> char* <'\"'>
+   <char> = #'[^\"]'
    ")
 
 (defn parse [source]
   (insta/transform
-    {:number (fn [& digits] [:number (int (apply str digits))])}
+    {:number (fn [& digits] [:number (int (apply str digits))])
+     :string (fn [& chars] [:string (apply str chars)])}
     (oi-program source)))
 
 (deftest simple-parses
-  (is (= (parse "42") [:start [:literal [:number 42]]])))
+  (is (= (parse "42") [:start [:literal [:number 42]]]))
+  (is (= (parse "\"foo\"") [:start [:literal [:string "foo"]]])))
