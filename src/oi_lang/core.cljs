bee12e8e53b4e8dad4af12183641be9de2df2efa
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2017-05-01 13:02:47 +0300

    Nicer errors

--- src/oi_lang/core.cljs
+++ src/oi_lang/core.cljs
@@ -4,22 +4,27 @@
 (enable-console-print!)
 
 (defn run [evt]
-  (let [code (-> js/document (.getElementById "code") .-value)
-        parse-tree (obj/parse code)
+  (let [code-input (-> js/document (.getElementById "code") .-value)
+        parse-tree (obj/parse code-input)
         parse-tree-output (-> js/document (.getElementById "parse-tree"))
-        _ (set! (.-value parse-tree-output) parse-tree)
-        runtime-representation (obj/ast->runtime* parse-tree)
         runtime-representation-output (-> js/document (.getElementById "runtime-representation"))
-        _ (set! (.-value runtime-representation-output) runtime-representation)
-        evaluated-representation (obj/eval* runtime-representation)
         evaluated-representation-output (-> js/document (.getElementById "evaluated"))
-        _ (set! (.-value evaluated-representation-output) evaluated-representation)
-        pretty-representation (obj/pretty evaluated-representation)
-        pretty-representation-output (.getElementById js/document "pretty-representation")
-        _ (set! (.-value pretty-representation-output) pretty-representation)]
-    ))
+        pretty-representation-output (.getElementById js/document "pretty-representation")]
+    (if (seq? parse-tree)
+      (set! (.-value parse-tree-output) parse-tree)
+      (do
+        (set! (.-value parse-tree-output) (str "Error: " (.stringify js/JSON (clj->js parse-tree))))
+        (set! (.-value runtime-representation-output) "")
+        (set! (.-value evaluated-representation-output) "")
+        (set! (.-value pretty-representation-output) "")))
+    (let [runtime-representation (obj/ast->runtime* parse-tree)
+          _ (set! (.-value runtime-representation-output) runtime-representation)
+          evaluated-representation (obj/eval* runtime-representation)
+          _ (set! (.-value evaluated-representation-output) evaluated-representation)
+          pretty-representation (obj/pretty evaluated-representation)
+          _ (set! (.-value pretty-representation-output) pretty-representation)])))
 
 (-> js/document
-    (.getElementById "compile")
-    (.addEventListener "click" run))
+    (.getElementById "code")
+    (.addEventListener "keyup" run))
 
