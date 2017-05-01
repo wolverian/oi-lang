f506e34a32f3b3f230578f5680a5b681329ce74b
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2017-05-01 17:31:03 +0300

    fix most tests

--- test/oi_lang/obj_tests.cljs
+++ test/oi_lang/obj_tests.cljs
@@ -1,28 +1,29 @@
 (ns oi-lang.obj-tests
-  (:require [cljs.test :as test :refer [is] :refer-macros [deftest]]))
+  (:require [cljs.test :as test :refer [is] :refer-macros [deftest]]
+            [oi-lang.obj :as oi]))
 
 (deftest simple-parses
-  (is (= (parse "42") [[:number 42]]))
-  (is (= (parse "\"foo\"") [[:string "foo"]]))
-  (is (= (parse "bar") [[:message "bar"]]))
-  (is (= (parse "bar()") [[:message "bar" [:arglist]]]))
-  (is (= (parse "bar(1)") [[:message "bar" [:arglist [:number 1]]]]))
-  (is (= (parse "bar(1, 2)") [[:message "bar" [:arglist [:number 1] [:number 2]]]]))
-  (is (= (parse "bar(1, 2, 3)") [[:message "bar" [:arglist [:number 1] [:number 2] [:number 3]]]]))
-  (is (= (parse "foo bar") [[:send [:message "foo"] [:message "bar"]]]))
-  (is (= (parse "foo < bar") [[:send [:message "foo"] [:message "<" [:arglist [:message "bar"]]]]]))
-  (is (= (parse "x := 42") [[:message "setSlot" [:arglist [:message "x"] [:number 42]]]])))
+  (is (= (oi/parse "42") [[:number 42]]))
+  (is (= (oi/parse "\"foo\"") [[:string "foo"]]))
+  (is (= (oi/parse "bar") [[:message "bar"]]))
+  (is (= (oi/parse "bar()") [[:message "bar" [:arglist]]]))
+  (is (= (oi/parse "bar(1)") [[:message "bar" [:arglist [:number 1]]]]))
+  (is (= (oi/parse "bar(1, 2)") [[:message "bar" [:arglist [:number 1] [:number 2]]]]))
+  (is (= (oi/parse "bar(1, 2, 3)") [[:message "bar" [:arglist [:number 1] [:number 2] [:number 3]]]]))
+  (is (= (oi/parse "foo bar") [[:send [:message "foo"] [:message "bar"]]]))
+  (is (= (oi/parse "foo < bar") [[:send [:message "foo"] [:message "<" [:arglist [:message "bar"]]]]]))
+  (is (= (oi/parse "x := 42") [[:message "setSlot" [:arglist [:message "x"] [:number 42]]]])))
 
 (deftest simple-ast->runtime*-tests
-  (is (oi-= (ast->runtime* (parse "42")) [(oi-number 42)]))
-  (is (oi-= (ast->runtime* (parse "42 < 22")) [oi-false]))
-  (is (oi-= (ast->runtime* (parse "2<1")) [oi-false]))
-  (is (oi-= (ast->runtime* (parse "1<2")) [oi-true]))
-  (is (oi-= (ast->runtime* (parse "42 > 22")) [oi-true])))
+  (is (oi/oi-= (oi/ast->runtime* (oi/parse "42")) [(oi/oi-number 42)]))
+  (is (oi/oi-= (oi/ast->runtime* (oi/parse "42 < 22")) [oi/oi-false]))
+  (is (oi/oi-= (oi/ast->runtime* (oi/parse "2<1")) [oi/oi-false]))
+  (is (oi/oi-= (oi/ast->runtime* (oi/parse "1<2")) [oi/oi-true]))
+  (is (oi/oi-= (oi/ast->runtime* (oi/parse "42 > 22")) [oi/oi-true])))
 
 (deftest list-ast->runtime*-tests
-  (is (oi-= (ast->runtime* (parse "list(1, 2, 3)")) [(oi-list (oi-number 1) (oi-number 2) (oi-number 3))]))
-  (is (oi-= (ast->runtime* (parse "x := list(1, 2, 3); x")) [(oi-list (oi-number 1) (oi-number 2) (oi-number 3))])))
+  (is (oi/oi-= (oi/ast->runtime* (oi/parse "list(1, 2, 3)")) [(oi/oi-list (oi/oi-number 1) (oi/oi-number 2) (oi/oi-number 3))]))
+  (is (oi/oi-= (oi/ast->runtime* (oi/parse "x := list(1, 2, 3); x")) [(oi/oi-list (oi/oi-number 1) (oi/oi-number 2) (oi/oi-number 3))])))
 
 (deftest simple-eval-tests
-  (is (oi-= (eval* (ast->runtime* (parse "42"))) [(oi-number 42)])))
+  (is (oi/oi-= (oi/eval* (oi/ast->runtime* (oi/parse "42"))) [(oi/oi-number 42)])))
