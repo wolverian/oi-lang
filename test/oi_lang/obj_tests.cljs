8420c6ff6638d56f3ee719049d60c85fb29c3bf3
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2017-05-03 16:18:25 +0300

    Refactor tests

--- test/oi_lang/obj_tests.cljs
+++ test/oi_lang/obj_tests.cljs
@@ -14,24 +14,18 @@
   (is (= (oi/parse "foo < bar") [[:send [:message "foo"] [:message "<" [:arglist [:message "bar"]]]]]))
   (is (= (oi/parse "x := 42") [[:message "setSlot" [:arglist [:message "x"] [:number 42]]]])))
 
-(defn string->runtime* [s] (-> s oi/parse oi/ast->runtime*))
-
-(deftest simple-ast->runtime*-tests
-  (is (oi/oi-= (string->runtime* "42") [(oi/oi-number 42)]))
-  (is (oi/oi-= (string->runtime* "42 < 22") [oi/oi-false]))
-  (is (oi/oi-= (string->runtime* "2<1") [oi/oi-false]))
-  (is (oi/oi-= (string->runtime* "1<2") [oi/oi-true]))
-  (is (oi/oi-= (string->runtime* "42 > 22") [oi/oi-true])))
-
-(deftest list-ast->runtime*-tests
-  (is (oi/oi-= (string->runtime* "list(1, 2, 3)")
-               [(oi/oi-list (oi/oi-number 1) (oi/oi-number 2) (oi/oi-number 3))]))
-  (is (oi/oi-= (string->runtime* "x := list(1, 2, 3); x")
-               [(oi/oi-list (oi/oi-number 1) (oi/oi-number 2) (oi/oi-number 3))])))
-
-(defn string->result* [s] (-> s string->runtime* oi/eval*))
+(defn string->result* [s] (-> s oi/parse oi/ast->runtime* oi/eval*))
 
 (deftest simple-eval-tests
   (is (oi/oi-= (string->result* "42") (oi/oi-number 42)))
+  (is (oi/oi-= (string->result* "42 < 22") oi/oi-false))
+  (is (oi/oi-= (string->result* "2<1") oi/oi-false))
+  (is (oi/oi-= (string->result* "1<2") oi/oi-true))
+  (is (oi/oi-= (string->result* "42 > 22") oi/oi-true))
+  (is (oi/oi-= (string->result* "42") (oi/oi-number 42)))
   (is (oi/oi-= (string->result* "list(1, 2, 3); 42") (oi/oi-number 42)))
-  (is (oi/oi-= (string->result* "list(1, 2, 3); 2<42") oi/oi-true)))
+  (is (oi/oi-= (string->result* "list(1, 2, 3); 2<42") oi/oi-true))
+  (is (oi/oi-= (string->result* "list(1, 2, 3)")
+               (oi/oi-list (oi/oi-number 1) (oi/oi-number 2) (oi/oi-number 3))))
+  (is (oi/oi-= (string->result* "x := list(1, 2, 3); x")
+               (oi/oi-list (oi/oi-number 1) (oi/oi-number 2) (oi/oi-number 3)))))
